3a2dbf8c640c4efe081dac141f49183d9fa4021c
Zhen Wu <zhenwu@Zhens-MacBook-Pro.local>
2017-12-31 16:30:47 -0500

    Update Traffic Light Detection

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -153,13 +153,13 @@ class WaypointUpdater(object):
             next_waypoint_index = self.next_waypoint(self.cur_pose, self.waypoints)
             # lookahead_waypoints = self.waypoints[next_waypoint_index:next_waypoint_index+LOOKAHEAD_WPS]
 
-            # if (next_waypoint_index + LOOKAHEAD_WPS < len(self.waypoints)):
+            if (next_waypoint_index + LOOKAHEAD_WPS < len(self.waypoints)):
 
-            #     lookahead_waypoints = self.waypoints[next_waypoint_index:next_waypoint_index+LOOKAHEAD_WPS]
+                lookahead_waypoints = self.waypoints[next_waypoint_index:next_waypoint_index+LOOKAHEAD_WPS]
 
-            # else:
+            else:
 
-            #     lookahead_waypoints = self.waypoints[next_waypoint_index:] + self.waypoints[:(next_waypoint_index+LOOKAHEAD_WPS - len(self.aypoints))] 
+                lookahead_waypoints = self.waypoints[next_waypoint_index:] + self.waypoints[:(next_waypoint_index+LOOKAHEAD_WPS - len(self.aypoints))] 
 
 
             if self.red_light_waypoint is None or self.red_light_waypoint < 0:

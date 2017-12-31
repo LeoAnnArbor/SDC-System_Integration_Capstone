eed13cec26369b073a8170dc9d692c8c6785e252
Zhen Wu <zhenwu@Zhens-MacBook-Pro.local>
2017-12-31 00:49:56 -0500

    Update Traffic Light Detection

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -25,7 +25,7 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
 MAX_DECEL = 0.5
 STOP_DIST = 5.0
-TARGET_SPEED_MPH = 20
+TARGET_SPEED_MPH = 10
 
 class WaypointUpdater(object):
     def __init__(self):
